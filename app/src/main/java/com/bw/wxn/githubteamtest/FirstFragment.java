8ace288fe54fdcb2d169f87dfdbb820031a05568
wangkun <1642212939@qq.com>
2017-09-27 14:48:03 +0800

    wk_第一个Fragment第一次创建

--- app/src/main/java/com/bw/wxn/githubteamtest/FirstFragment.java
+++ app/src/main/java/com/bw/wxn/githubteamtest/FirstFragment.java
@@ -1,8 +1,20 @@
 package com.bw.wxn.githubteamtest;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by wxn on 2017/9/27.
  */
 
-public class FirstFragment {
+public class FirstFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
