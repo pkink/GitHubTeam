643b07ca000c5c0af90967a7a31a66dabb19da9d
wangchengen <2502794096@qq.com>
2017-09-27 14:45:09 +0800

    搭建框架

---
+++ app/src/main/java/com/bw/wxn/githubteamtest/SecondFragment.java
@@ -0,0 +1,8 @@
+package com.bw.wxn.githubteamtest;
+
+/**
+ * Created by wxn on 2017/9/27.
+ */
+
+public class SecondFragment {
+}
---
+++ app/src/main/java/com/bw/wxn/githubteamtest/model/model02.java
@@ -0,0 +1,8 @@
+package com.bw.wxn.githubteamtest.model;
+
+/**
+ * Created by wxn on 2017/9/27.
+ */
+
+public class model02 {
+}
